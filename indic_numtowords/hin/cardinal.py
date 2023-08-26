0e5f8270d562cb4b90a734242a52022da6a46215
DevMehendale <deovrat.mehendale@gmail.com>
2023-08-26 12:18:49 +0530

    Variations added for Hindi

--- indic_numtowords/hin/cardinal.py
+++ indic_numtowords/hin/cardinal.py
@@ -2,26 +2,34 @@ from indic_numtowords.hin.data.nums import exceptions_dict
 from indic_numtowords.hin.data.nums import direct_dict
 from indic_numtowords.hin.data.nums import higher_dict
 from indic_numtowords.hin.data.nums import variations_dict
+from indic_numtowords.hin.data.nums import symbols_dict
 
 from indic_numtowords.hin.utils import combine
 
 def convert(num):
-    num_str = str(num).strip()
+    final_word_list = []
+    word_list1=[""]
+    word_list = [""]
+
+    if str(num)[0]=='-':
+        word_list = combine(word_list, symbols_dict['-'])
+        word_list1 = combine(word_list1, symbols_dict['-'])
+        num_str = str(num)[1:].strip()
+    else:
+        num_str = str(num).strip()
+        
     if num_str in exceptions_dict:
         return exceptions_dict[num_str]
+    
+    
+    for i in num_str:
+        new_list = direct_dict[i]
+        word_list1 = combine(word_list1, new_list)
+    final_word_list.extend(word_list1)
+
     num_str = num_str.lstrip('0')
     n = len(num_str)
-    word_list = [""]
-    final_word_list = []
-    
 
-    if n > 9:
-        #output individually
-        for i in num_str:
-            new_list = direct_dict[i]
-            word_list = combine(word_list, new_list)
-        final_word_list.extend(word_list)
-        return final_word_list
 
     if n == 9 or n == 8:
         #crore case
@@ -48,6 +56,12 @@ def convert(num):
     if n == 5 or n == 4:
         #thousands case
         temp_num = num_str[:-3]
+
+        if len(temp_num)==1:                             
+            word_list_alt = word_list.copy()
+            word_list_alt = combine(word_list_alt, direct_dict[num_str[:-2]])
+            word_list_alt = combine(word_list_alt, higher_dict[2]+[''])
+
         lis1 = direct_dict[temp_num]
         lis2 = higher_dict[3]
         inter_list = combine(lis1, lis2)
@@ -62,6 +76,7 @@ def convert(num):
         lis1 = direct_dict[temp_num]
         lis2 = higher_dict[2]
         inter_list = combine(lis1, lis2)
+        inter_list.extend(direct_dict[temp_num])
         if num_str in variations_dict:
             final_word_list.extend(combine(word_list, variations_dict[num_str]))
         word_list = combine(word_list, inter_list)
@@ -74,6 +89,12 @@ def convert(num):
         lis2 = direct_dict[num_str]
         lis1 = word_list
         word_list = combine(lis1, lis2)
+
+        if 'word_list_alt' in locals():                      
+            lis2 = direct_dict[num_str]
+            lis1 = word_list_alt
+            word_list_alt = combine(lis1, lis2)
+            word_list.extend(word_list_alt)
 
     final_word_list = word_list + final_word_list
     return [l.strip() for l in final_word_list]
--- indic_numtowords/hin/utils.py
+++ indic_numtowords/hin/utils.py
@@ -2,5 +2,5 @@ def combine(lis1, lis2, seperator = " "):
     inter_list = []
     for i in lis1:
         for j in lis2:
-            inter_list.append(i + seperator + j)
+            inter_list.append((i + seperator + j).rstrip())
     return inter_list
